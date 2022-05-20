4b97f5ff478866062f4a47a1bd0cad92f379c4ac
enzomolloy <enzomolloy@gmail.com>
2022-05-20 14:27:54 -0300

    Repaso 3

---
+++ Assets/PrefabCloner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrefabCloner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Text InputCloneAmount;
+    public Text displayClonesLeft;
+    public InputField IField;
+    public int counter;
+
+
+    void Start()
+    {
+        counter = 0;
+        displayClonesLeft.text = "";
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ClonePrefab()
+    {
+        if (InputCloneAmount.text != "")
+
+            IField.readOnly = true;
+        int cloneAmount = int.Parse(InputCloneAmount.text);
+
+        if (counter < cloneAmount)
+        {
+            Instantiate(prefab);
+            counter++;
+            displayClonesLeft.text = (cloneAmount - counter).ToString();
+        }
+        else
+        {
+            displayClonesLeft.text = "No hay más";
+        }
+      
+
+        else
+        {
+            displayClonesLeft.text = "ERROR. Ingrese un valor";
+        }
+     
+    }
+}
